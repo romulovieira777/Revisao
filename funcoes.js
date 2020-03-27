e96427c99b4451b398462f2237d23776b0cd6b9f
Romulo Vieira <romulo.vieira777@gmail.com>
2020-03-26 21:51:08 -0300

    Mudando

---
+++ funcoes.js
@@ -0,0 +1,25 @@
+alert("Aula de JavaScript");
+//comentário 1 linha
+
+/*linha1
+linha2
+linha3
+Várias linhas
+...*/
+
+/*var numero = 2020;
+var nome = "Romulo";
+var carac = '';
+var x = "Noite";
+var y = " Dia";
+var teste = x + y;
+alert(teste);*/
+
+var x = 5;
+var y = 2;
+if(x == y){
+    alert("Iguais");
+}
+else{
+    
+}
